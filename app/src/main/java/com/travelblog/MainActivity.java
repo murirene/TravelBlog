ddaa75d61333dcf7f9d412a5d0b65ca261dc9d9a
murirene <rene.murillo@ymail.com>
2023-05-07 00:29:51 -0700

    Modern Android App Development Educative course Initial upload.

--- app/src/main/java/com/travelblog/MainActivity.java
+++ app/src/main/java/com/travelblog/MainActivity.java
@@ -1,6 +1,8 @@
 package com.travelblog;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainTextview = findViewById(R.id.mainTextView);
+        mainTextview.setText(R.string.educative_io);
     }
 
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
 }
